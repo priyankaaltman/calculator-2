da5ff6bcbaadc7cbc9ccf2a4a8a3a615fd08cae3
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-19 15:46:26 -0700

    added checks for number of arguments

--- calculator.py
+++ calculator.py
@@ -6,25 +6,24 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-# No setup
-# repeat forever:
-#     read input
-#     tokenize input
-#     if the first token is "q":
-#         quit
-#     else:
-#         decide which math function to call based on first token
-
-# Your code goes here
-
 exit = False
 while exit == False:
     selection =input("Enter a computation: ")
     tokens = selection.split(" ")
 
     operation = tokens[0]
+    if operation == "q" or operation == "quit":
+        exit = True
+        print("Exiting...")
+        continue
+
     num1 = float(tokens[1])
-    num2 = float(tokens[2])
+
+    if len(tokens) == 3:
+        num2 = float(tokens[2])
+
+    if len(tokens) == 4:
+        num3 = float(tokens[3])
 
     if operation == "+":
         print(add(num1, num2))
@@ -43,13 +42,7 @@ while exit == False:
     elif operation == "mod":
         print(mod(num1, num2))
     elif operation == "mult+":
-        num3 = float(tokens[3])
         print(add_mult(num1, num2, num3))
     elif operation == "cubes+":
         print(add_cubes(num1, num2))
-    elif operation == "q" or operation == "quit":
-        exit = True
-        print("Exiting...")
-
-
 
